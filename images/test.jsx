5178ea3c50bfb62cdbd239be4b1b727e27c225a8
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-05-19 20:26:13 -0500

    pagination

--- images/test.jsx
+++ images/test.jsx
@@ -1,10 +1,4 @@
 import { useState } from 'react';
-import * as React from 'react';
-import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem';
-import Stack from '@mui/material/Stack';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 
 // THIS IS A SEPARATE FILE USED FOR TESTING PURPOSES
@@ -15,17 +9,6 @@ export default function Test() {
 
   return (
     <>
-    <Stack spacing={2}>
-      <Pagination
-        count={10}
-        renderItem={(item) => (
-          <PaginationItem
-            slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-            {...item}
-          />
-        )}
-      />
-    </Stack>
     </>
 );
 }
