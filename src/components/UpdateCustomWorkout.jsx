3c95c495f12e370c822392c3e3a0073308e33f53
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-06-04 14:31:18 -0500

    UC day fix

--- src/components/UpdateCustomWorkout.jsx
+++ src/components/UpdateCustomWorkout.jsx
@@ -27,9 +27,10 @@ export default function UpdateCustomWorkout() {
         equipment: "",
         rep_range: '',
         weight_range: '',
-        workout_variation: "",
+        variation: "", 
         day: ""
     })
+    
 
     // set the state to hold the endpoint to update
     const [updateEnd, setUpdateEnd] = useState()
@@ -37,49 +38,15 @@ export default function UpdateCustomWorkout() {
     // set state for update form when on screen
     const [updateForm, setUpdateForm] = useState(false)
 
-    const [workoutsReady, setWorkoutsReady] = useState(false);
+    const [daysReady, setIsDaysReady] = useState(false)
+
+    // enables / disables the left day button
+    const [isLeftEnabled, setIsLeftEnabled] = useState(false);
+
+    // enables / disables the right day button
+    const [isRightEnabled, setIsRightEnabled] = useState(false);
 
     ////////////////////////////////////////////////////////
-    // state for workout data from api call
-    const [workoutData, setWorkoutData] = useState()
-
-    // state for workout name
-    const [variationName, setvariationName] = useState([])
-
-    // set state for the selected users choice
-    const [variationChoice, setVariationChoice] = useState()
-
-    //sets state for counter to go through the array choices
-    const [variationCounter, setVariationCounter] = useState(0)
-    /////////////////////////////////////////////////////////
-
-    /////////////////////////////////////////////////////////
-    // state for muscle data from api call
-    const [muscleData, setMuscleData] = useState()
-
-    // state for muscle group
-    const [muscleName, setMuscleName] = useState([])
-
-    // set state for muscle choice
-    const [muscleChoice, setMuscleChoice] = useState()
-
-    //set state for muscle counter
-    const [muscleCounter, setMuscleCounter] = useState(0)
-    /////////////////////////////////////////////////////////
-
-    /////////////////////////////////////////////////////////
-    // state for equipment data from api call
-    const [equipmentData, setEquipmentData] = useState()
-
-    // state for equipment Name
-    const [equipmentName, setEquipmentName] = useState()
-
-    // set state for equipment choice
-    const [equipmentChoice, setEquipmentChoice] = useState()
-
-    // set state for equipment counter
-    const [equipmentCounter, setEqipmentCounter] = useState(0)
-    /////////////////////////////////////////////////////////
 
     /////////////////////////////////////////////////////////
     // state for day data from api call
@@ -92,51 +59,11 @@ export default function UpdateCustomWorkout() {
     const [dayChoice, setDayChoice] = useState()
 
     // set state for date counter
-    const [dayCounter, setDayCounter] = useState(0)
+    const [dayCounter, setDayCounter] = useState(-1)
     /////////////////////////////////////////////////////////
 
     // calls the functions on initial page render
     useEffect(() => {
-        // calls the api that has translations 
-        //(or so I thought. I thought it was all english but it wasn't)
-        const renderVariations = async () => {
-            const res = await fetch('https://wger.de/api/v2/exercise-translation/?limit=50&offset=0')
-            if (res.ok) {
-                const data = await res.json();
-                setWorkoutData(data);
-                setWorkoutsReady(true);
-            }
-            // if not error out
-            else {
-                console.error("Couldn't get the products :(")
-            }
-        }
-
-        // calls the api that has the muscle groups
-        const renderMuscles = async () => {
-            const res = await fetch('https://wger.de/api/v2/muscle/')
-            if (res.ok) {
-                const data = await res.json();
-                setMuscleData(data);
-            }
-            // if not error out
-            else {
-                console.error("Couldn't get the products :(")
-            }
-        }
-
-        // calls the api that has the workout equipment
-        const renderEquipment = async () => {
-            const res = await fetch('https://wger.de/api/v2/equipment/')
-            if (res.ok) {
-                const data = await res.json();
-                setEquipmentData(data);
-            }
-            // if not error out
-            else {
-                console.error("Couldn't get the products :(")
-            }
-        }
 
         // calls the api that has the day data
         const renderDay = async () => {
@@ -144,6 +71,7 @@ export default function UpdateCustomWorkout() {
             if (res.ok) {
                 const data = await res.json();
                 setDayData(data);
+                setIsDaysReady(true);
             }
             // if not error out
             else {
@@ -151,11 +79,34 @@ export default function UpdateCustomWorkout() {
             }
         }
 
-        renderVariations();
-        renderMuscles();
-        renderEquipment();
         renderDay();
     }, []);
+
+    // Update button state based on dayCounter
+    useEffect(() => {
+        // if api call is successfull
+        if (daysReady) {
+            // disable button if the counter is at 0
+            if (dayCounter === 0) {
+                setIsLeftEnabled(true);
+            } else {
+                setIsLeftEnabled(false);
+            }
+
+            // diable the counter if its at 6
+            if (dayCounter === 6) {
+                setIsRightEnabled(true);
+            } else {
+                setIsRightEnabled(false);
+            }
+
+            // set the current day choice
+            setDayChoice(dayData.results[dayCounter].day_of_week)
+        }
+        else {
+            setIsLeftEnabled(true);
+        }
+    }, [dayCounter]);
 
     // Handle form submission for updating a workout
     const handleUpdate = async (event) => {
@@ -166,11 +117,11 @@ export default function UpdateCustomWorkout() {
                 'Content-Type': 'application/json' // Indicates the content 
             },
             body: JSON.stringify({
-                "muscle_group": muscleChoice,
-                "equipment": equipmentChoice,
+                "muscle_group": userInputs.muscle_group,
+                "equipment": userInputs.equipment,
                 "rep_range": userInputs.rep_range,
                 "weight_range": userInputs.weight_range,
-                "workout_variation": variationChoice,
+                "variation": userInputs.variation,
                 "day": dayChoice
             }),
         });
@@ -184,7 +135,7 @@ export default function UpdateCustomWorkout() {
                 "equipment": "",
                 "rep_range": '',
                 "weight_range": '',
-                "workout_variation": "",
+                "variation": "",
                 "day": ""
             })
             // resets the user chosen workout number to delete
@@ -204,48 +155,6 @@ export default function UpdateCustomWorkout() {
 
     // transforms api request data into arrays to look through
     const CreateCustomWorkoutBox = () => {
-
-        //////////WORKOUT VARIATION//////////
-        let filter = new Set();
-        for (let i = 0; i < workoutData.results.length; i++) {
-            if (workoutData.results[i].language === 2) {
-                filter.add(workoutData.results[i].name)
-            }
-        }
-
-        // Convert Set to array before setting state
-        const workoutNamesArray = [...filter];
-        setvariationName(workoutNamesArray);
-        //////////////////////////////////////
-
-        //////////MUSCLE GROUP//////////
-        let filter2 = new Set();
-        for (let j = 0; j < muscleData.results.length; j++) {
-            if (muscleData.results[j].name_en.length === 0) {
-                filter2.add(`${muscleData.results[j].name}`)
-            }
-            else {
-                filter2.add(`${muscleData.results[j].name}(${muscleData.results[j].name_en})`)
-            }
-        }
-
-        // Convert Set to array before setting state
-        const workoutNamesArray2 = [...filter2];
-        setMuscleName(workoutNamesArray2);
-        ////////////////////////////////
-
-
-        //////////WORKOUT EQUIPMENT//////////
-        let filter3 = new Set();
-        for (let k = 0; k < equipmentData.results.length; k++) {
-            filter3.add(equipmentData.results[k].name)
-        }
-        // Convert Set to array before setting state
-        const workoutNamesArray3 = [...filter3];
-        setEquipmentName(workoutNamesArray3);
-        /////////////////////////////////////
-
-
         //////////DAYS OF THE WEEK////////////
         // holds the data when going through the for loop
         let copy = [];
@@ -260,137 +169,20 @@ export default function UpdateCustomWorkout() {
         /////////////////////////////////////
     }
 
-    // has the controls for moving left through the workouts
-    const previousWorkoutVariation = () => {
-
-        // make a copy of the array from the api
-        let copy = variationName;
-
-        // if the associated counter is 0 return
-        if (variationCounter === 0) {
-            return;
-        }
-
-        // increment the state of the counter by 1
-        setVariationCounter(variationCounter - 1);
-
-        // set the choice of the user as the index at the current
-        // value of the counter
-        setVariationChoice(copy[variationCounter]);
-    }
-
-    // has the controls for moving left through the workouts
-    const nextWorkoutVariation = () => {
-
-        // make a copy of the array from the api
-        let copy = variationName;
-
-        // if the associated counter is at the end of the array return
-        if (variationCounter === copy.length - 1) {
-            return;
-        }
-
-        // decrement the state of the counter by 1
-        setVariationCounter(variationCounter + 1);
-
-        // set the choice of the user as the index at the current
-        // value of the counter
-        setVariationChoice(copy[variationCounter]);
-    }
-
-    // controls moving left for equiment
-    const previousEquipment = () => {
-
-        // create copy of equipment array contents
-        let copy = equipmentName;
-
-        // if start of array return
-        if (equipmentCounter === 0) {
-            return;
-        }
-
-        // decrement counter by 1
-        setEqipmentCounter(equipmentCounter - 1);
-
-        // set user choice to index location in copy at counter
-        setEquipmentChoice(copy[equipmentCounter]);
-    }
-
-    // controls moving right for equipment
-    const nextEquipment = () => {
-
-        // create copy of eqipment array
-        let copy = equipmentName;
-
-        // if end of array return
-        if (equipmentCounter === copy.length - 1) {
-            return;
-        }
-
-        // increment equipment counter by 1
-        setEqipmentCounter(equipmentCounter + 1);
-
-        // set user item to location in copy by equipment ounter
-        setEquipmentChoice(copy[equipmentCounter]);
-    }
-
-    // controls moving left for muscles
-    const previousMuscle = () => {
-
-        // create copy of muscle array
-        let copy = muscleName;
-
-        // if start of array return
-        if (muscleCounter === 0) {
-            return;
-        }
-
-        // decrement muscle counter by one
-        setMuscleCounter(muscleCounter - 1);
-
-        // set user muscle choice to location of counter in the copy
-        setMuscleChoice(copy[muscleCounter]);
-    }
-
-    // controls moving right for muscles
-    const nextMuscle = () => {
-        let copy = muscleName;
-
-        if (muscleCounter === copy.length - 1) {
-            return;
-        }
-        setMuscleCounter(muscleCounter + 1);
-        setMuscleChoice(copy[muscleCounter]);
-    }
-
     // controls moving right through days
     const previousDay = () => {
-
-        // create copy of day array
-        let copy = dayName;
-
-        //if start of array return
-        if (dayCounter === 0) {
-            return;
+        if (dayCounter > 0) {
+            setDayCounter(dayCounter - 1);
+            setDayChoice(dayData.results[dayCounter - 1].day_of_week);
         }
-
-        // decrement day counter by one
-        setDayCounter(dayCounter - 1)
-
-        // set user day choice to location of counter in the copy
-        setDayChoice(copy[dayCounter])
     }
 
     // controls moving left through days
     const nextDay = () => {
-        let copy = dayName;
-
-        if (dayCounter === copy.length) {
-            return;
+        if (dayCounter < dayData.results.length - 1) {
+            setDayCounter(dayCounter + 1);
+            setDayChoice(dayData.results[dayCounter + 1].day_of_week);
         }
-
-        setDayCounter(dayCounter + 1);
-        setDayChoice(copy[dayCounter])
     }
 
     // Handle changes in form inputs and displays them on screen as they happen
@@ -415,7 +207,7 @@ export default function UpdateCustomWorkout() {
             <br />
             {/* if the workouts api call is successful move on OR
                 inform the user the form is still loading */}
-            {workoutsReady ? (
+            {daysReady ? (
                 <>
                     {/* Start of the form */}
                     <Form onSubmit={handleUpdate}>
@@ -440,21 +232,15 @@ export default function UpdateCustomWorkout() {
                                  prompt the user to show the buttons*/}
                             {updateForm ? (
                                 <>
-                                    {/* day choice segment */}
+                                    {/* day of the week segment */}
                                     <Form.Label value={dayChoice}>
                                         <div className="center">
                                             <br />
                                             <Form.Label htmlFor="inputDay_of_The_Week">Day Of The Week:</Form.Label>
                                             <br />
-                                            <button type="button" onClick={previousDay}><ArrowBackIcon /></button>
-                                            {dayChoice ? (
-                                                <>
-                                                    {dayChoice}
-                                                </>
-                                            ) : (
-                                                'Please choose a button'
-                                            )}
-                                            <button type="button" onClick={nextDay}><ArrowForwardIcon /></button>
+                                            <button type="button" disabled={isLeftEnabled} onClick={previousDay}><ArrowBackIcon /></button>
+                                            {dayChoice ? (<>{dayChoice}</>) : ('Please select a button')}
+                                            <button type="button" disabled={isRightEnabled} onClick={nextDay}><ArrowForwardIcon /></button>
                                         </div>
                                     </Form.Label>
 
@@ -503,6 +289,12 @@ export default function UpdateCustomWorkout() {
                                 <FormControl sx={{ gridColumn: '1/-1' }}>
                                     <FormLabel>Rep Range: </FormLabel>
                                     <Input onChange={handleChange} type='text' name='rep_range' value={userInputs.rep_range || ""} placeholder="Enter your rep range" />
+                                </FormControl>
+
+                                {/* input box segment */}
+                                <FormControl sx={{ gridColumn: '1/-1' }}>
+                                    <Form.Label htmlFor="name">Workout ID:</Form.Label>
+                                    <Input type="text" name="name" value={updateEnd} onChange={handleUpdateValue} placeholder="Enter the workout ID to delete" />
                                 </FormControl>
 
                                 {/* Bottom of the form and submit button */}
