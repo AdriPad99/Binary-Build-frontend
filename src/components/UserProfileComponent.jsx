043a6e80ec863ec77f7a0f3b30766eab15e9c557
AdriPad99 <adrian.mpadilla99@gmail.com>
2024-07-17 17:21:13 -0500

    profile WIP

--- src/components/UserProfileComponent.jsx
+++ src/components/UserProfileComponent.jsx
@@ -21,6 +21,8 @@ export default function UserProfileComponent() {
 
     const { token } = useContext(AuthContext);
 
+    const [userInfo, setUserInfo] = useState();
+
     // Combined state for all user data
     const [userData, setUserData] = useState({
         age: "",
@@ -39,6 +41,7 @@ export default function UserProfileComponent() {
                 const response = await fetch('https://capstone-db.onrender.com/signup/1');
                 if (response.ok) {
                     const data = await response.json();
+                    setUserInfo(data);
                     setUserData({
                         email: data.email || '',
                         firstName: data.first_name || '',
@@ -48,6 +51,17 @@ export default function UserProfileComponent() {
                         gender: data.gender || '',
                         height: data.height || '',
                         weight: data.weight || '',
+                        target_weight: data.target_weight || '',
+                        target_body_fat_percentage: data.target_body_fat_percentage || '',
+                        fitness_level: data.fitness_level || '',
+                        daily_activity_level: data.daily_activity_level || '',
+                        pref_workout_types: data.pref_workout_types || '',
+                        pref_workout_duration: data.pref_workout_duration || '',
+                        available_equipment: data.available_equipment || '',
+                        chest: data.chest || '',
+                        waist: data.waist || '',
+                        hips: data.hips || '',
+                        amnt_workouts_completed: data.amnt_workouts_completed || '',
                     });
                 } else {
                     console.error("Failed to fetch user data:", response.statusText);
@@ -61,40 +75,45 @@ export default function UserProfileComponent() {
         getUserData();
     }, [token]); // Assuming token is correctly triggering useEffect when it changes
 
+    const test = () => {
+        console.log(userInfo)
+        console.log(userData)
+    }
+
 
     return (
         <>
-
-        <div className='id'>
-            <Card
-                sx={{
-                    width: 320,
-                    maxWidth: '100%',
-                    boxShadow: 'lg',
-                }}
-            >
-                <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
-                    <Avatar src="/static/images/avatar/1.jpg" sx={{ '--Avatar-size': '4rem' }} />
-                    <Typography level="title-lg">{userData.firstName} {userData.lastName} ({userData.username})</Typography>
-                    <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
-                        Hello, this is my bio and I am a Student currently attending the Coding Temple program.
-                        <hr />
-                        Contact Me: {userData.email}
-                    </Typography>
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            gap: 2,
-                            mt: 2,
-                            '& > button': { borderRadius: '2rem' },
-                        }}
-                    >
-                    </Box>
-                </CardContent>
-                <CardOverflow sx={{ bgcolor: 'background.level1' }}>
-                </CardOverflow>
-            </Card>
-        </div>
+        <button onClick={test}>test</button>
+            <div className='id'>
+                <Card
+                    sx={{
+                        width: 320,
+                        maxWidth: '100%',
+                        boxShadow: 'lg',
+                    }}
+                >
+                    <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
+                        <Avatar src="/static/images/avatar/1.jpg" sx={{ '--Avatar-size': '4rem' }} />
+                        <Typography level="title-lg">{userData.firstName} {userData.lastName} ({userData.username})</Typography>
+                        <Typography level="body-sm" sx={{ maxWidth: '24ch' }}>
+                            Hello, this is my bio and I am a Student currently attending the Coding Temple program.
+                            <hr />
+                            Contact Me: {userData.email}
+                        </Typography>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                gap: 2,
+                                mt: 2,
+                                '& > button': { borderRadius: '2rem' },
+                            }}
+                        >
+                        </Box>
+                    </CardContent>
+                    <CardOverflow sx={{ bgcolor: 'background.level1' }}>
+                    </CardOverflow>
+                </Card>
+            </div>
         </>
     )
 }
