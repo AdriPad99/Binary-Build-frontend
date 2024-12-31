28c4134c1203e6aa514b0dd3a7c88ec4f88c1f3c
Adrian Padilla <adrian@Adrians-MBP.attlocal.net>
2024-12-31 12:53:52 -0600

    wip36

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import AuthContext from './AuthContext';
 
 // auth provider holds shared state and state methods
-export const AuthProvider = ({ children }) => { // userID wip35
+export const AuthProvider = ({ children }) => { // userID wip36
     const [token, setToken] = useState(() => localStorage.getItem('token'));
 
     const [counter, setCounter] = useState(() => parseInt(localStorage.getItem('counter'), 10) || 0)
